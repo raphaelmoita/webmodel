80a70121aff263764c6fe3cbbcd592e3fd7d22eb
Raphael Moita <raphael.moita@gmail.com>
2019-03-17 11:23:37 +0100

    fixing wirer

--- acceptance-test/src/main/java/org/moita/sb2/acceptance/base/FixtureWirer.java
+++ acceptance-test/src/main/java/org/moita/sb2/acceptance/base/FixtureWirer.java
@@ -1,8 +1,6 @@
 package org.moita.sb2.acceptance.base;
 
-import org.moita.sb2.WebmodelApplication;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -15,24 +13,20 @@ public class FixtureWirer {
 
     private AutowireCapableBeanFactory beanFactory;
 
-    public void wire(Fixture fixture) { //FIXME: ClassNotFoundException ??? Why ???
-        ApplicationContext applicationContext = new WebmodelApplication().run(new String[]{"123"});
+//    public void wire(Fixture fixture) { //FIXME: ClassNotFoundException ??? Why ???
+//        ApplicationContext applicationContext = new WebmodelApplication().run(new String[]{"123"});
+//
+//        if (beanFactory == null) {
+//            beanFactory = applicationContext.getAutowireCapableBeanFactory();
+//        }
+//        beanFactory.autowireBeanProperties(fixture, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false);
+//        beanFactory.autowireBean(fixture);
+//        beanFactory.initializeBean(fixture, fixture.getClass().getName());
+//    }
 
-        if (beanFactory == null) {
-            beanFactory = applicationContext.getAutowireCapableBeanFactory();
-        }
-        beanFactory.autowireBeanProperties(fixture, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false);
-        beanFactory.autowireBean(fixture);
-        beanFactory.initializeBean(fixture, fixture.getClass().getName());
-    }
-
-    public void wire_(Fixture fixture) {
+    public void wire(Fixture fixture) {
         if (beanFactory == null) {
             AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(FixtureWirer.class);
-            context.getEnvironment().setActiveProfiles("fitnesse");
-            context.register(FixtureWirer.class);
-            context.scan("org.moita.sb2");
-            context.refresh();
             beanFactory = context.getAutowireCapableBeanFactory();
         }
         beanFactory.autowireBeanProperties(fixture, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false);
--- app/src/main/java/org/moita/sb2/WebmodelApplication.java
+++ app/src/main/java/org/moita/sb2/WebmodelApplication.java
@@ -1,30 +1,18 @@
 package org.moita.sb2;
 
-import org.moita.sb2.model.Employee;
-import org.moita.sb2.service.SimpleEmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class WebmodelApplication {
 
-	@Autowired
-	private SimpleEmployeeService employeeService;
-
-	@PostConstruct
-	public void execute() {
-		Employee employee = new Employee();
-		employee.setId(1);
-		employee.setName("Raphael");
-//		employeeService.addEmployee(employee);
-//		employeeService.listEmployees().forEach(System.out::println);
-//		employeeService.insertDummy();
-	}
+    public ApplicationContext run(String[] args)
+    {
+        return SpringApplication.run(WebmodelApplication.class, args);
+    }
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebmodelApplication.class, args);
+		new WebmodelApplication().run(args);
 	}
 }
