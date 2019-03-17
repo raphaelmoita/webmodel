86129c091eef85dd5791cdea5c6caebc94da4c35
Raphael Moita <raphael.moita@gmail.com>
2019-03-17 22:11:51 +0100

    two different ways to set up profiles

--- acceptance-test/bin/start.sh
+++ acceptance-test/bin/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#export SPRING_PROFILES_ACTIVE=fitnesse
-
 export JAVA_HOME=/home/rmoita/softwares/jdk1.8.0_181/
 export MVN_HOME=/home/rmoita/softwares/apache-maven-3.5.2/
 
@@ -13,4 +11,8 @@ for jar in $(ls target/dependencies/*.jar); do
     CLASSPATH="${CLASSPATH}:${jar}"
 done
 
-SPRING_PROFILES_ACTIVE=fitnesse ${JAVA_HOME}/bin/java -jar bin/fitnesse-standalone.jar -p 8888 -d bin/ -e 0 -l bin/logs
+# this
+SPRING_PROFILES_ACTIVE=default ${JAVA_HOME}/bin/java -jar bin/fitnesse-standalone.jar -p 8888 -d bin/ -e 0 -l bin/logs
+# or
+# export SPRING_PROFILES_ACTIVE=fitnesse
+# ${JAVA_HOME}/bin/java -jar bin/fitnesse-standalone.jar -p 8888 -d bin/ -e 0 -l bin/logs
